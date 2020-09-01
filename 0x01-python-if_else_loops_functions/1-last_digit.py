073ccd16e8310ef13dfe4b77e6af8a386e3281c5
Lilita1762 <lilibeth.tabares@gmail.com>
2020-09-01 17:13:00 +0000

    Last digit of a number. Fixed pep8

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-last_digit = (number % 10)
-if (last_digit > 5):
-    print("Last digit of", number, "is", last_digit, "and is greater than 5")
-elif (last_digit == 0):
-    print("Last digit of", number, "is", last_digit, "and is 0")
+lst_dg = (number % 10) + 0
+if (lst_dg > 5):
+    print("Last digit of", number, "is", lst_dg, "and is greater than 5")
+elif (lst_dg == 0):
+    print("Last digit of", number, "is", lst_dg, "and is 0")
 else:
-    print("Last digit of", number, "is", last_digit, "and is less than 6 and not 0")
+    print("Last digit of", number, "is", lst_dg, "and is less than 6 and not 0")
