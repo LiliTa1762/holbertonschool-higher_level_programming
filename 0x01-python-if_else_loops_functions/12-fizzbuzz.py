3e23c3282f65f63c7b6abfa8b6208826539a3433
Lilita1762 <lilibeth.tabares@gmail.com>
2020-09-02 01:43:29 +0000

    Function that prints Fizz Buzz. Pep fixed

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -2,10 +2,10 @@
 def fizzbuzz():
     for i in range(1, 101):
         if i % 3 == 0 and i % 5 == 0:
-            print("fizzbuzz", end=" ")
+            print("Fizzbuzz", end=" ")
         elif i % 3 == 0:
-            print("fizz", end=" ")
+            print("Fizz", end=" ")
         elif i % 5 == 0:
-            print("buzz", end=" ")
+            print("Buzz", end=" ")
         else:
             print(i, end=" ")
