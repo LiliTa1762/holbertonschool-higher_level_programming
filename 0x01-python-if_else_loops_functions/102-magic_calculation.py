d994b42ebad8933adf5f69a7452ba277b8d7fcd0
Lilita1762 <lilibeth.tabares@gmail.com>
2020-09-02 20:14:06 +0000

    Function that does the same Py bytecode

--- 0x01-python-if_else_loops_functions/102-magic_calculation.py
+++ 0x01-python-if_else_loops_functions/102-magic_calculation.py
@@ -1,10 +1,8 @@
-import dis
-
-
+#!/usr/bin/python3
 def magic_calculation(a, b, c):
     if a < b:
-        return c
+        return(c)
     elif c > b:
-        return a + b
-    return a * b - c
-dis.dis(magic_calculation)
+        return(a + b)
+    else:
+        return(a * b - c)
