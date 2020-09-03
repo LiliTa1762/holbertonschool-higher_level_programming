165828c221063724e4dc06fbd312e5a54d400e60
Lilita1762 <lilibeth.tabares@gmail.com>
2020-09-03 22:26:47 +0000

    Program that prints the number of a list with the argv. Adding format and other

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
+from sys import argv
 if __name__ == "__main__":
-    from sys import argv
-if (len(argv) == 1):
-    print("{} arguments. ".format(len(argv) - 1))
-elif (len(argv) == 2):
-    print("{} argument: ".format(len(argv) - 1))
-else:
-    print("{} arguments: ".format(len(argv) - 1))
-for i in range(1, len(argv)):
-    print("{}: {}".format(i, argv[i]))
+# No arguments
+    if (len(argv) == 1):
+        print("{} arguments. ".format(len(argv) - 1))
+# Only one argument
+    elif (len(argv) == 2):
+        print("{} argument: ".format(len(argv) - 1))
+    else:
+        print("{} arguments: ".format(len(argv) - 1))
+    for i in range(1, len(argv)):
+        print("{}: {}".format(i, argv[i]))
