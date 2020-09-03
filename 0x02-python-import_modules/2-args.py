a1ae29002ab3e1cc8987d18bcc9b6748e61cb825
Lilita1762 <lilibeth.tabares@gmail.com>
2020-09-03 22:08:17 +0000

    Program that prints the number of a list with the argv

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-     import sys
+    import sys
 # Argv is 0 (whithout the name of the file)
 if (len(sys.argv) == 1):
-    print((len(sys.argv) -1), "arguments.")
+    print((len(sys.argv) - 1), "arguments.")
 # Argv is 1
 elif (len(sys.argv) == 2):
-    print((len(sys.argv) -1), "argument: ")
+    print((len(sys.argv) - 1), "argument: ")
 else:
-    print((len(sys.argv) -1), "arguments: ")
+    print((len(sys.argv) - 1), "arguments: ")
 for i in range(1, len(sys.argv)):
     print("{}: {}".format(i, sys.argv[i]))
