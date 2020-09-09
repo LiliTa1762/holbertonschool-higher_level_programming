5cb798e6264826660ff85286d7acadaf52371cf0
Lilita1762 <lilibeth.tabares@gmail.com>
2020-09-09 02:55:58 +0000

    Deletes the item at a idx

---
+++ 0x03-python-data_structures/11-delete_at.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def delete_at(my_list=[], idx=0):
+    if my_list:
+        if idx < 0 and idx >= len(my_list):
+            return None
+    del my_list[idx]
+    return my_list
