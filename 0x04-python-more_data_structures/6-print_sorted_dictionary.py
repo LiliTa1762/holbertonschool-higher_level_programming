cdb97325a76bd8117576b616b8f08ed38df66d82
Lilita1762 <lilibeth.tabares@gmail.com>
2020-09-10 22:32:05 +0000

    Prints a dict by ordered keys. Fixing errors

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 def print_sorted_dictionary(a_dictionary):
-    for x, y in sorted(a_dictionary.items()):
-        print(x, y)
-    return a_dictionary
+    if a_dictionary:
+        for x, y in sorted(a_dictionary.items()):
+            print(x, y)
+        return a_dictionary
