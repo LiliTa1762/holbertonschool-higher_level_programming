b238b8d7d110661d6480d4ddd1da69ef10fc4cd5
Lilita1762 <lilibeth.tabares@gmail.com>
2020-09-21 16:48:42 +0000

    Prints an int with format

---
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def safe_print_integer(value):
+    while True:
+        try:
+            if value(x) is int:
+                return (True)
+        except:
+            return (False)
