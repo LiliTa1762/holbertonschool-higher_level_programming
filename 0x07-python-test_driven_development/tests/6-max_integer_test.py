5a1c2531bb474303ba51fecb999a11933bd909cb
Lilita1762 <lilibeth.tabares@gmail.com>
2020-09-26 03:13:21 +0000

    Creating Unittests, fixing pep8

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -4,6 +4,7 @@
 import unittest
 max_integer = __import__('6-max_integer').max_integer
 
+
 class TestMaxInteger(unittest.TestCase):
     """Test cases for the function max_integer"""
     def test_max_int(self):
@@ -12,20 +13,24 @@ class TestMaxInteger(unittest.TestCase):
         message = None
         self.assertIsNone(max_integer(list), message)
 
+
 def test_one_element(self):
         """Tests for only one number in the list"""
         list = [1]
         self.assertEqual(max_integer(list), 1)
+
 
 def test_positive_end(self):
         """Tests for all positive with max at end"""
         l = [2, 10, 8, 36, 14, 50]
         self.assertEqual(max_integer(l), 50)
 
+
 def test_positive_middle(self):
         """Tests for all positive with max in middle"""
         m = [2, 10, 8, 360, 14, 50]
         self.assertEqual(max_integer(m), 360)
+
 
 if __name__ == '__main__':
     unittest.main()
