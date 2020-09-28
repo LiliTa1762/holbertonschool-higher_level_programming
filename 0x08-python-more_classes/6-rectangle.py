477b6558216967e02cba0c20757e0f3d823874c9
Lilita1762 <lilibeth.tabares@gmail.com>
2020-09-28 22:11:18 +0000

    Counting instances. Pep8 fixed

--- 0x08-python-more_classes/6-rectangle.py
+++ 0x08-python-more_classes/6-rectangle.py
@@ -4,7 +4,7 @@
 
 class Rectangle:
     """Class Rectangle"""
-    number_of_instances= 0
+    number_of_instances = 0
 
     def __init__(self, width=0, height=0):
         """Instantiation object"""
