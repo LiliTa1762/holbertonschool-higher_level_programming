c865be625d1b3955728758996f367f320e427a73
Lilita1762 <lilibeth.tabares@gmail.com>
2020-09-28 17:18:20 +0000

    Class with private instance att

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+"""Defines a rectangle"""
+
+
+class Rectangle:
+    """Class with private instance att"""
+    def __init__(self, width=0, height=0):
+        self.__width = width
+        self.__height = height
+
+    @property
+    def width(self):
+        """Retrieve a property"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """Set if this conditions"""
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+
+        if value < 0:
+            raise ValueError("width must be >= 0")
+
+        self.__width = value
+
+
+    @property
+    def height(self):
+        """Retrieve a property"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """Set if this conditions"""
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+
+        if value < 0:
+            raise ValueError("height must be >= 0")
+
+        self.__height = value
