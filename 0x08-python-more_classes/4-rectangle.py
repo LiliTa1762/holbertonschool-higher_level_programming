a0043af67ad6f6c975f3d215b9f86901a8a148ad
Lilita1762 <lilibeth.tabares@gmail.com>
2020-09-29 03:46:47 +0000

    Adding repr()

--- 0x08-python-more_classes/4-rectangle.py
+++ 0x08-python-more_classes/4-rectangle.py
@@ -43,7 +43,7 @@ class Rectangle:
 
     def perimeter(self):
         """Define Perimeter of rentangle"""
-        if self.__width == 0 and self.__height == 0:
+        if self.__width == 0 or self.__height == 0:
             return 0
         else:
             return 2 * (self.__height + self.__width)
