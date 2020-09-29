686483ed53fa826c253d5c9681cf4dc4e59a0e76
Lilita1762 <lilibeth.tabares@gmail.com>
2020-09-29 03:32:48 +0000

    Adding instances att

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -17,10 +17,10 @@ class Rectangle:
     @width.setter
     def width(self, value):
         """Set if this conditions"""
-        if type(value) is not int:
+        if type(width) is not int:
             raise TypeError("width must be an integer")
 
-        if value < 0:
+        if width < 0:
             raise ValueError("width must be >= 0")
 
         self.__width = value
@@ -33,10 +33,10 @@ class Rectangle:
     @height.setter
     def height(self, value):
         """Set if this conditions"""
-        if type(value) is not int:
+        if type(height) is not int:
             raise TypeError("height must be an integer")
 
-        if value < 0:
+        if height < 0:
             raise ValueError("height must be >= 0")
 
         self.__height = value
