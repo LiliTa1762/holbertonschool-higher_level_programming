3294780018fbabb88890580c9428dd65a4753b2f
Lilita1762 <lilibeth.tabares@gmail.com>
2020-09-29 03:37:17 +0000

    Adding instances att. Fixed

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -6,8 +6,8 @@ class Rectangle:
     """Class with private instance att"""
     def __init__(self, width=0, height=0):
         """Instantiation object"""
-        self.__width = width
-        self.__height = height
+        self.width = width
+        self.height = height
 
     @property
     def width(self):
@@ -17,10 +17,10 @@ class Rectangle:
     @width.setter
     def width(self, value):
         """Set if this conditions"""
-        if type(width) is not int:
+        if type(value) is not int:
             raise TypeError("width must be an integer")
 
-        if width < 0:
+        if value < 0:
             raise ValueError("width must be >= 0")
 
         self.__width = value
@@ -32,11 +32,11 @@ class Rectangle:
 
     @height.setter
     def height(self, value):
-        """Set if this conditions"""
-        if type(height) is not int:
+        """Set if these conditions"""
+        if type(value) is not int:
             raise TypeError("height must be an integer")
 
-        if height < 0:
+        if value < 0:
             raise ValueError("height must be >= 0")
 
         self.__height = value
