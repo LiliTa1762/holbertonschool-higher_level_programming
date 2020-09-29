8a01564f7d03865cec2c62cb12fd8d745e446ff5
Lilita1762 <lilibeth.tabares@gmail.com>
2020-09-29 03:40:11 +0000

    Adding str(). Fixed

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -6,8 +6,8 @@ class Rectangle:
     """Class Rectangle"""
     def __init__(self, width=0, height=0):
         """Instantiation object"""
-        self.__width = width
-        self.__height = height
+        self.width = width
+        self.height = height
 
     @property
     def width(self):
