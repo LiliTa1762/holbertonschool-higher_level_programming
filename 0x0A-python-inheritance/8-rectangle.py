9cf560596f66ed36aa6f4f565658ac2445e1037c
Lilita1762 <lilibeth.tabares@gmail.com>
2020-10-06 23:14:17 +0000

    Instantiation of a rectangle

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -13,9 +13,9 @@ class BaseGeometry:
     def integer_validator(self, name, value):
         """Next public instance method"""
         if type(value) is not int:
-            raise TypeError("{} must be an integer".format(name))
+            raise TypeError("{:s} must be an integer".format(name))
         if value <= 0:
-            raise ValueError("{} must be greater than 0".format(name))
+            raise ValueError("{:s} must be greater than 0".format(name))
 
 
 class Rectangle(BaseGeometry):
