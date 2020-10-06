2c7cb9936a45b0f2d15950003c6fef4cec2173b5
Lilita1762 <lilibeth.tabares@gmail.com>
2020-10-06 22:56:53 +0000

    Subclass with instantiation

---
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+Class with a public instance method
+"""
+
+
+class BaseGeometry:
+    """Class with a public instance method"""
+    def area(self):
+        """Public instance method"""
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """Next public instance method"""
+        if type(value) is not int:
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
+
+
+class Rectangle(BaseGeometry):
+    """Subclass"""
+    def __init__(self, width, height):
+        """Instantiation with two arg"""
+        self.integer_validator("width", width)
+        self.__width = width
+        self.integer_validator("height", height)
+        self.__height = height
