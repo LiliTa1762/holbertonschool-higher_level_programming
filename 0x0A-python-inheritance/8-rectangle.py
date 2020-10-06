23b6a6bdb4cf3952dc5f223ada2784eca207190e
Lilita1762 <lilibeth.tabares@gmail.com>
2020-10-06 23:26:34 +0000

    Instantiation of a rectangle

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -6,22 +6,31 @@ Class with a public instance method
 
 class BaseGeometry:
     """Class with a public instance method"""
+
     def area(self):
         """Public instance method"""
-        raise Exception("area() is not implemented")
+        raise Exception('area() is not implemented')
 
     def integer_validator(self, name, value):
-        """Next public instance method"""
+        '''
+        Next public instance method
+        '''
         if type(value) is not int:
             raise TypeError("{:s} must be an integer".format(name))
+
         if value <= 0:
             raise ValueError("{:s} must be greater than 0".format(name))
 
 
 class Rectangle(BaseGeometry):
-    """Subclass"""
+    '''
+    Subclass.
+    '''
+
     def __init__(self, width, height):
-        """Instantiation with two arg"""
+        '''
+        initialization with two arg.
+        '''
         self.integer_validator("width", width)
         self.__width = width
         self.integer_validator("height", height)
