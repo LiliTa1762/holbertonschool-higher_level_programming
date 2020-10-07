a19bb8e9b66e007e0be642bfdce40dc51510962c
Lilita1762 <lilibeth.tabares@gmail.com>
2020-10-07 17:00:01 +0000

    Read txt

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -6,6 +6,6 @@ Function that reads a text file
 
 def read_file(filename=""):
     """Read a file"""
-    with open(filename, encoding="utf-8") as f:
+    with open(filename, "r", encoding="utf-8") as f:
         a_read = f.read()
         print(a_read, end="")
