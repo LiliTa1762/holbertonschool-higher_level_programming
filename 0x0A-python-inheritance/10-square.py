177bb60968b50bfb434714769aa4eceb01e22ac6
Lilita1762 <lilibeth.tabares@gmail.com>
2020-10-07 01:09:05 +0000

    Square 1, module imported

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -4,45 +4,7 @@ Class with a public instance method
 """
 
 
-class BaseGeometry:
-    """Class with a public instance method"""
-
-    def area(self):
-        """Public instance method"""
-        raise Exception('area() is not implemented')
-
-    def integer_validator(self, name, value):
-        '''
-        Next public instance method
-        '''
-        if type(value) is not int:
-            raise TypeError("{:s} must be an integer".format(name))
-
-        if value <= 0:
-            raise ValueError("{:s} must be greater than 0".format(name))
-
-
-class Rectangle(BaseGeometry):
-    '''
-    Subclass.
-    '''
-
-    def __init__(self, width, height):
-        '''
-        initialization with two arg.
-        '''
-        self.integer_validator("width", width)
-        self.__width = width
-        self.integer_validator("height", height)
-        self.__height = height
-
-    def area(self):
-        """Area method implemented for rectangle"""
-        return self.__width * self.__height
-
-    def __str__(self):
-        """str implemented"""
-        return("[Rectangle] {:d}/{:d}".format(self.__width, self.__height))
+Rectangle = __import__('9-rectangle').Rectangle
 
 
 class Square(Rectangle):
