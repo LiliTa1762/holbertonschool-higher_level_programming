43f3309f8bb051f0b8468be90d1f97c796f56628
Lilita1762 <lilibeth.tabares@gmail.com>
2020-10-07 22:02:11 +0000

    Read n lines. Fixed

--- 0x0B-python-input_output/2-read_lines.py
+++ 0x0B-python-input_output/2-read_lines.py
@@ -7,6 +7,11 @@ Function that reads n lines of a file
 def read_lines(filename="", nb_lines=0):
     """Read n lines"""
     with open(filename, "r", encoding="utf-8") as f:
-        if nb_lines <= 0 or nb_lines >= nb_lines:
-            a_read = f.read()
-            print(a_read, end="")
+        if nb_lines <= 0:
+            print(f.read(), end="")
+            return
+
+        for i, x in enumerate(f):
+            if i == nb_lines:
+                break
+            print(x, end="")
