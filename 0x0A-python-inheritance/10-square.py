f846b41e754c9f1e6e0715ba27479915403dac0e
Lilita1762 <lilibeth.tabares@gmail.com>
2020-10-07 00:05:43 +0000

    Square class

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,58 @@
+#!/usr/bin/python3
+"""
+Class with a public instance method
+"""
+
+
+class BaseGeometry:
+    """Class with a public instance method"""
+
+    def area(self):
+        """Public instance method"""
+        raise Exception('area() is not implemented')
+
+    def integer_validator(self, name, value):
+        '''
+        Next public instance method
+        '''
+        if type(value) is not int:
+            raise TypeError("{:s} must be an integer".format(name))
+
+        if value <= 0:
+            raise ValueError("{:s} must be greater than 0".format(name))
+
+
+class Rectangle(BaseGeometry):
+    '''
+    Subclass.
+    '''
+
+    def __init__(self, width, height):
+        '''
+        initialization with two arg.
+        '''
+        self.integer_validator("width", width)
+        self.__width = width
+        self.integer_validator("height", height)
+        self.__height = height
+
+    def area(self):
+        return self.__width * self.__height
+
+    def __str__(self):
+        return("[Rectangle] {:d}/{:d}".format(self.__width, self.__height))
+
+
+class Square(Rectangle):
+    """Representation of a square"""
+
+    def __init__(self, size):
+        """Instantiation with size"""
+        self.integer_validator("size", size)
+        self.__size = size
+
+    def area(self):
+        return(self.__size * self.__size)
+
+    def __str__(self):
+        return("[Rectangle] {:d}/{:d}".format(self.__size, self.__size))
