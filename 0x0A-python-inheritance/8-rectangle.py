b5506fe1dfb8a08d981a337cd8cd3b19ffb953eb
Lilita1762 <lilibeth.tabares@gmail.com>
2020-10-07 01:05:44 +0000

    Importing module

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -6,23 +6,6 @@ Class with a public instance method
 
 BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
-class BaseGeometry:
-    """Class with a public instance method"""
-
-    def area(self):
-        """Public instance method"""
-        raise Exception('area() is not implemented')
-
-    def integer_validator(self, name, value):
-        '''
-        Next public instance method
-        '''
-        if type(value) is not int:
-            raise TypeError("{:s} must be an integer".format(name))
-
-        if value <= 0:
-            raise ValueError("{:s} must be greater than 0".format(name))
-
 
 class Rectangle(BaseGeometry):
     '''
