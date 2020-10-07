1073d73663ca7a5c327e4904b6dc569b41cb0461
Lilita1762 <lilibeth.tabares@gmail.com>
2020-10-07 22:03:17 +0000

    Read n lines. Fixed

--- 0x0B-python-input_output/1-number_of_lines.py
+++ 0x0B-python-input_output/1-number_of_lines.py
@@ -5,6 +5,7 @@
 def number_of_lines(filename=""):
     """return the number of lines"""
     with open(filename, "r", encoding="utf-8") as f:
+        i = 0
         for i, l in enumerate(f):
             """"i for the count and l for run into function"""
             i += 1
