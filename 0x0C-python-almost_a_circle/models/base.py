bfe483457ba3ad6fb89420463ca9025830a4f3d3
Lilita1762 <lilibeth.tabares@gmail.com>
2020-10-13 18:33:29 +0000

    changing return

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -58,7 +58,7 @@ class Base:
     def to_json_string(list_dictionaries):
         """Static method, returns the JSON representation"""
         if list_dictionaries is None:
-            list_dictionaries = []
+            return("[]")
         else:
             return json.dumps(list_dictionaries)
 
