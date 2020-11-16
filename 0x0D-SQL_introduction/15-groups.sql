bc6942a333e3a1d92ef59231919451b97e23ca1a
Lilita1762 <lilibeth.tabares@gmail.com>
2020-11-16 23:58:47 +0000

    Putting all de cmd in one line

--- 0x0D-SQL_introduction/15-groups.sql
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -1,3 +1,2 @@
 --  Lists the number of records with the same score in the table second_table
-SELECT score FROM second_table;
-SELECT COUNT(score) 'number' FROM second_table;
+SELECT score, COUNT(*) 'number' FROM second_table GROUP BY score ORDER BY score DESC;
