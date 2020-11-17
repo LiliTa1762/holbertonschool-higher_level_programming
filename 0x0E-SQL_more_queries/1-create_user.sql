35e54f1fbed02c3d74db7c0ceeb54a04934627b0
Lilita1762 <lilibeth.tabares@gmail.com>
2020-11-17 21:32:23 +0000

    Create a user with privileges. Another way

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,4 +1,3 @@
 -- Create the user of the MySQL server
-CREATE USER IF NOT EXISTS user_0d_1 @localhost IDENTIFIED BY user_0d_1_pwd;
-GRANT ALL PRIVILEGES ON * . * TO user_0d_1 @localhost;
+GRANT ALL PRIVILEGES ON * . * TO user_0d_1 @localhost IDENTIFIED BY 'user_0d_1_pwd';
 FLUSH PRIVILEGES;
