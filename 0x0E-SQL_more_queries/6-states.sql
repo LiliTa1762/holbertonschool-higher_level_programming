843aac32400b153b5f18cba0f1aebea2791c6335
Lilita1762 <lilibeth.tabares@gmail.com>
2020-11-17 22:33:02 +0000

    Table and Database

---
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -0,0 +1,3 @@
+-- Creates the database hbtn_0d_usa and the table states in that database
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS states (id INT NOT NULL UNIQUE PRIMARY KEY AUTO_INCREMENTED, name VARCHAR(256) NOT NULL);
