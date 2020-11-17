0e7392b1b0aa946a8021121cb480c35951821b82
Lilita1762 <lilibeth.tabares@gmail.com>
2020-11-17 21:18:43 +0000

    Create first user with privileges

---
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -0,0 +1,4 @@
+-- Create the user of the MySQL server
+CREATE USER IF NOT EXISTS user_0d_1 @localhost IDENTIFIED BY user_0d_1_pwd;
+GRANT ALL PRIVILEGES ON * . * TO user_0d_1 @localhost;
+FLUSH PRIVILEGES;
