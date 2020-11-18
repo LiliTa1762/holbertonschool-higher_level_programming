38f5756b6f6192cc365e15a8f4aba43928ae5025
Lilita1762 <lilibeth.tabares@gmail.com>
2020-11-18 02:58:48 +0000

    Using Inner join

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,5 +1,6 @@
 --  Lists all cities contained in the database hbtn_0d_usa
-SELECT id, name
-FROM cities
-	NATURAL JOIN states
-ORDER BY id;
+-- We have to truly specify each element, otherwise is ambiguous
+SELECT cities.id, cities.name, states.name
+FROM cities INNER JOIN states
+ON cities.state_id = states.id
+ORDER BY cities.id;
