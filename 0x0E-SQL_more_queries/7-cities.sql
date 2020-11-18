4ff568e0e5a154c7ec3bdf17d5dc8e838de036ba
Lilita1762 <lilibeth.tabares@gmail.com>
2020-11-18 00:28:35 +0000

    City table. Foreign key

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -1,5 +1,5 @@
 -- the database hbtn_0d_usa and the table cities
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
-CREATE TABLE IF NOT EXISTS cities (id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-state_id INT NOT NULL, FOREIGN KEY(state_id) REFERENCES states(id),
+CREATE TABLE IF NOT EXISTS cities (id INT NOT NULL AUTO_INCREMENT PRIMARY KEY(id),
+state_id INT NOT NULL, FOREIGN KEY(state_id) REFERENCES states(state_id),
 name VARCHAR(256) NOT NULL);
