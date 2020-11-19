153105d72fbd3dcf0f3e2b24c9a808028d864269
Lilita1762 <lilibeth.tabares@gmail.com>
2020-11-19 01:18:46 +0000

    Genre ID for all shows. Fixed

--- 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -1,7 +1,9 @@
 -- Lists all shows contained in hbtn_0d_tvshows
+-- If a show doesn’t have a genre, display NULL
 SELECT tv_shows.title, tv_show_genres.genre_id
-FROM tv_shows FULL OUTER JOIN tv_show_genres
-
+FROM tv_shows 
+LEFT JOIN tv_show_genres
+ON tv_shows.id = tv_show_genres.show_id
 ORDER BY 
 	tv_shows.title ASC,
  	tv_show_genres.genre_id ASC;
