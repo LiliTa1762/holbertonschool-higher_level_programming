851fb22f031da0e879bc03f1de76e9da24bf15bc
Lilita1762 <lilibeth.tabares@gmail.com>
2020-12-11 21:57:41 +0000

    Get all states, connect method

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+Getting all states of a DB
+"""
+
+
+import MySQLdb as mdb
+import sys
+
+if __name__ == "__main__":
+    user = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+    con = mdb.connect('localhost', user, password, database)
+
+    with con:
+        cur = con.cursor()
+        cur.execute("SELECT * FROM states ORDER BY states.id")
+
+        rows = cur.fetchall()
+
+        for row in rows:
+            print(row)
