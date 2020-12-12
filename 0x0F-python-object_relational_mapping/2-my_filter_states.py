758d9600785092af293ec9dfae6affb5837be433
Lilita1762 <lilibeth.tabares@gmail.com>
2020-12-12 21:32:28 +0000

    Filter with a argv4

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+Filter the states with user input
+"""
+
+
+import MySQLdb as mdb
+import sys
+
+user = sys.argv[1]
+password = sys.argv[2]
+database = sys.argv[3]
+con = mdb.connect('localhost', user, password, database)
+
+with con:
+    cur = con.cursor()
+    cur.execute("SELECT * FROM states WHERE name = '{}' ORDER BY states.id".format(sys.argv[4]))
+
+    rows = cur.fetchall()
+
+    for row in rows:
+        print(row)
