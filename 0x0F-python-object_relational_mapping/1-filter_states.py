9703d25ebe50207b834d100fd03fae1a92b1cc4c
Lilita1762 <lilibeth.tabares@gmail.com>
2020-12-12 02:47:29 +0000

    Filtering with N%

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -20,25 +20,3 @@ with con:
 
     for row in rows:
         print(row)
-
-"""
-import mysql.connector
-
-mydb = mysql.connector.connect(
-    host="localhost",
-    user="yourusername",
-    password="yourpassword",
-    database="mydatabase"
-)
-
-mycursor = mydb.cursor()
-
-sql = "SELECT * FROM customers WHERE name ='N"
-
-mycursor.execute(sql)
-
-myresult = mycursor.fetchall()
-
-for x in myresult:
-  print(x)
-  """
