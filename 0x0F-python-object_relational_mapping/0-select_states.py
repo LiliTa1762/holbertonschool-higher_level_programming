76f953d0e8e5d2bac09e735da5d0916b769c1736
Lilita1762 <lilibeth.tabares@gmail.com>
2020-12-12 21:37:42 +0000

    All the states using with

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -7,16 +7,18 @@ Getting all states of a DB
 import MySQLdb as mdb
 import sys
 
-user = sys.argv[1]
-password = sys.argv[2]
-database = sys.argv[3]
-con = mdb.connect('localhost', user, password, database)
 
-with con:
-    cur = con.cursor()
-    cur.execute("SELECT * FROM states ORDER BY states.id")
+if __name__ == "__main__":
+    user = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+    con = mdb.connect('localhost', user, password, database)
 
-    rows = cur.fetchall()
+    with con:
+        cur = con.cursor()
+        cur.execute("SELECT * FROM states ORDER BY states.id")
 
-    for row in rows:
-	    print(row)
+        rows = cur.fetchall()
+
+        for row in rows:
+            print(row)
