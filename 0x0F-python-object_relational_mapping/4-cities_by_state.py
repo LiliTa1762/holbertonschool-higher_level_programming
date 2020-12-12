c037dd4e6225fc8e07230bb708810254c3d24174
Lilita1762 <lilibeth.tabares@gmail.com>
2020-12-12 21:52:10 +0000

    New DB using Inner Join

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -7,17 +7,21 @@ Getting all states of a DB
 import MySQLdb as mdb
 import sys
 
-user = sys.argv[1]
-password = sys.argv[2]
-database = sys.argv[3]
-con = mdb.connect('localhost', user, password, database)
 
-with con:
-    cur = con.cursor()
-    cur.execute(
-		"SELECT cities.id, cities.name, states.name FROM cities INNER JOIN states ON cities.state_id = states.id ORDER BY cities.id ASC")
+if __name__ == "__main__":
+    user = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+    con = mdb.connect('localhost', user, password, database)
 
-    rows = cur.fetchall()
+    with con:
+        cur = con.cursor()
+        cur.execute(
+            "SELECT cities.id, cities.name, states.name FROM cities \
+            INNER JOIN states ON cities.state_id = states.id \
+            ORDER BY cities.id ASC")
 
-    for row in rows:
-	    print(row)
+        rows = cur.fetchall()
+
+        for row in rows:
+            print(row)
