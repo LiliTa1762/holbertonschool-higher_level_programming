f0c461a7230690ec159847de1b66dc105a4a4644
Lilita1762 <lilibeth.tabares@gmail.com>
2020-12-12 21:33:50 +0000

    Avoid SQL injection

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+Filter the states with user input
+"""
+
+
+import MySQLdb as mdb
+import sys
+
+if __name__ == "__main__":
+    user = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+    con = mdb.connect('localhost', user , password, database)
+
+    
+    cur = con.cursor()
+    cur.execute("SELECT * FROM states WHERE name = %s ORDER BY id ASC", (sys.argv[4], ))
+
+    rows = cur.fetchall()
+
+    for row in rows:
+        print(row)
+
+    cur.close()
+    con.close()
