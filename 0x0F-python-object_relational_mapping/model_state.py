1696dbe541991c402ed2ddf468e3301b6e4cad14
Lilita1762 <lilibeth.tabares@gmail.com>
2020-12-13 02:49:53 +0000

    First state model. MYSQLALCHEMY

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Class definition of State 
+"""Class definition of State
 """
 
 
@@ -9,7 +9,9 @@ from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
 
+
 class State(Base):
+    """Class of state"""
     __tablename__ = 'states'
     id = Column(Integer, autoincrement=True, nullable=False, primary_key=True)
-    name =  Column(String(128), nullable=False)
+    name = Column(String(128), nullable=False)
