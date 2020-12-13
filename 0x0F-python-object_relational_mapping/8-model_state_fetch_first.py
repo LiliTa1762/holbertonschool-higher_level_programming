6e9c4a2584cfc870eec2a2f0d374bbfad43d1ea4
Lilita1762 <lilibeth.tabares@gmail.com>
2020-12-13 15:56:04 +0000

    show the first state

---
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""Class definition of State
+"""
+
+
+from model_state import Base, State
+from sqlalchemy import Column, Integer, String
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+import sys
+
+
+if __name__ == "__main__":
+    engine = create_engine(
+        'mysql+mysqldb://{}:{}@localhost/{}'.
+        format(sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+
+    con = engine.connect()
+
+    rs = con.execute("SELECT * from states ORDER BY id ASC LIMIT 1")
+
+    row = rs.fetchall()
+    for rows in row:
+        print('{:d}: {:s}'.format(rows[0], rows[1]))
