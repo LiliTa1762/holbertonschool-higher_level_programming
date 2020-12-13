452ea949d5645e785e1aa509073ac7c0489d5169
Lilita1762 <lilibeth.tabares@gmail.com>
2020-12-13 16:27:30 +0000

    show the first state

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -21,5 +21,8 @@ if __name__ == "__main__":
     rs = con.execute("SELECT * from states ORDER BY id ASC LIMIT 1")
 
     row = rs.fetchall()
-    for rows in row:
-        print('{:d}: {:s}'.format(rows[0], rows[1]))
+    if rs:
+        for rows in row:
+            print('{:d}: {:s}'.format(rows[0], rows[1]))
+    else:
+        print()
