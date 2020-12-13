f52bf617692ad11d33fceddb990626675e04cb02
Lilita1762 <lilibeth.tabares@gmail.com>
2020-12-13 15:09:52 +0000

    All states via SQLAlchemy

---
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""Class definition of State
+"""
+
+
+from model_state import Base, State
+from sqlalchemy import Column, Integer, String
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+import sys
+
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+
+    con = engine.connect()
+
+    rs = con.execute("SELECT * from states ORDER BY id ASC")
+
+    row = rs.fetchone()
+    for row in rs:
+        print('{:d}: {:s}'.format(row[0], row[1]))
