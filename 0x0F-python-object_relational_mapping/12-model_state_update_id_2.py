6397618ffdc2cfeeb2f3ab14be1bf09107cf0399
Lilita1762 <lilibeth.tabares@gmail.com>
2020-12-13 22:31:42 +0000

    Update a state, using get

---
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""Adds a State object to the database
+"""
+
+
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+import sys
+
+
+if __name__ == "__main__":
+    engine = create_engine(
+        'mysql+mysqldb://{}:{}@localhost/{}'.
+        format(sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+    Session = sessionmaker(bind=engine)
+
+    session = Session()
+
+    con = session.query(State).get(2)
+
+    con.name = "New Mexico"
+
+    session.commit()
+
+    session.close()
