ae8fea4edb39e373763c86e782ccc5240adad5f9
Lilita1762 <lilibeth.tabares@gmail.com>
2021-01-11 17:33:55 +0000

    What's my status?. Using methods of response and data(html)

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Script to fetch a webpage"""
+
+
+import urllib.request
+
+
+if __name__ == "__main__":
+    with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
+        html = response.read()
+        print("Body response:")
+        print("    - type: {}".format(type(html)))
+        print("    - content: {}".format(html))
+        print("    - utf8 content: {}".format(html.decode('utf-8')))
