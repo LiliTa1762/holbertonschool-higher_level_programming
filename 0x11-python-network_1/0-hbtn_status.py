1cf6f31c25e47037f58e0f9e5e33b64792928fed
Lilita1762 <lilibeth.tabares@gmail.com>
2021-01-11 17:56:14 +0000

    What's my status?. Using methods of response and data(html)

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -11,4 +11,4 @@ if __name__ == "__main__":
         print("Body response:")
         print("    - type: {}".format(type(html)))
         print("    - content: {}".format(html))
-        print("    - utf8 content: {}".format(html.decode('utf-8')))
+        print("    - utf8 content: {}".format(html.decode()))
