133dc1512accccda9540a599e4556762ba6b2726
Lilita1762 <lilibeth.tabares@gmail.com>
2021-01-11 17:30:26 +0000

    pep8 fixed

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -7,6 +7,6 @@ import sys
 
 
 if __name__ == "__main__":
-	with urllib.request.urlopen(sys.argv[1]) as response:
-		html = response.read()
-		print(response.getheader("X-Request-Id"))
+    with urllib.request.urlopen(sys.argv[1]) as response:
+        html = response.read()
+        print(response.getheader("X-Request-Id"))
