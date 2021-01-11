fadb4a72f3e53f1185c484aad1af1c9cc08f571d
Lilita1762 <lilibeth.tabares@gmail.com>
2021-01-11 23:42:01 +0000

    POST request with requests. Adding comma

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -7,6 +7,6 @@ import sys
 
 
 if __name__ == "__main__":
-    payload = {'email': sys.argv2}
-    r = requests.POST(sys.argv[1] data=payload)
+    payload = {'email': sys.argv[2]}
+    r = requests.post(sys.argv[1], data=payload)
     print(r.test)
