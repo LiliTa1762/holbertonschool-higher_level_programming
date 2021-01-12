dd62183169a3b4495171e5eb0336cc938f89dcb6
Lilita1762 <lilibeth.tabares@gmail.com>
2021-01-12 03:24:25 +0000

    POST an email, with urlib. FIXED

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -3,13 +3,17 @@
 
 
 import urllib.request
+import urllib.parse
 import sys
 
 
 if __name__ == "__main__":
-    try:
-        with urllib.request.urlopen(sys.argv[1]) as t:
-            sol = t.read()
-            print(sol.decode('utf-8'))
-    except urllib.error.HTTPError as e:
-        print("Error code: {}".format(e.code))
+    url = sys.argv[1]
+    values = {'email': sys.argv[2]}
+
+    data = urllib.parse.urlencode(values)
+    data = data.encode()
+    req = urllib.request.Request(url, data)
+    with urllib.request.urlopen(req) as rt:
+        d = rt.read()
+        print(d.decode('utf-8'))
