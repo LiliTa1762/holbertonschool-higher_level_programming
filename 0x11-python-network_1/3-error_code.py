4c4b4e72ce8966d6e18289411ddc646e952209aa
Lilita1762 <lilibeth.tabares@gmail.com>
2021-01-12 03:14:42 +0000

    names of variable

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -5,10 +5,10 @@
 import urllib.request
 import sys
 
-
-try:
-    with urllib.request.urlopen(sys.argv[1]) as t:
-        sol = t.read()
-        print(sol.decode('utf-8'))
-except urllib.error.HTTPError as e:
-    print("Error code: {}".format(e.code))
+if __name__ == "__main__":
+    try:
+        with urllib.request.urlopen(sys.argv[1]) as t:
+            sol = t.read()
+            print(sol.decode('utf-8'))
+    except urllib.error.HTTPError as e:
+        print("Error code: {}".format(e.code))
