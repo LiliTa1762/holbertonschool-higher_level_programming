a241f773c37b8f9037ed1c5899ae0a10afc510aa
Lilita1762 <lilibeth.tabares@gmail.com>
2021-01-12 03:00:18 +0000

    Response body and manage error. urllib. fixed execution

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -5,10 +5,10 @@
 import urllib.request
 import sys
 
-
-try:
-    with urllib.request.urlopen(sys.argv[1]) as response:
-        html = response.read()
-        print(html.decode("utf-8"))
-    except urllib.error.HTTPError as e:
-        print("Error code: {}".format(e.code))
+if __name__ == "__main__":
+    try:
+        with urllib.request.urlopen(sys.argv[1]) as response:
+            html = response.read()
+            print(html.decode("utf-8"))
+        except urllib.error.HTTPError as e:
+            print("Error code: {}".format(e.code))
