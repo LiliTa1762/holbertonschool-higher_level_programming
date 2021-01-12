92a84c6913498f39cd0f0af88576511dac4a2920
Lilita1762 <lilibeth.tabares@gmail.com>
2021-01-12 03:19:00 +0000

    POST an email, with urlib

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Take in a URL, send a POST request and display a response"""
+
+
+import urllib.request
+import sys
+
+
+if __name__ == "__main__":
+    try:
+        with urllib.request.urlopen(sys.argv[1]) as t:
+            sol = t.read()
+            print(sol.decode('utf-8'))
+    except urllib.error.HTTPError as e:
+        print("error code:{} ".format(e.code))
