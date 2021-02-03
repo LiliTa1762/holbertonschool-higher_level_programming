de2c10021db3a8fd43b7c5259337a619d0e6c273
Lilita1762 <lilibeth.tabares@gmail.com>
2021-02-03 04:14:23 +0000

    Fixing add function

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,14 +1,10 @@
 #!/usr/bin/node
 
 function add(a, b) {
-  a = parseInt(process.argv[2]);
-  b = parseInt(process.argv[3]);
-
-  if (a !== null && b !== null) {
-    console.log(a + b);
-  }
-  else {
-    console.log('NaN');
-  }
+  return a + b;
 }
-add(process.argv[2], process.argv[3]);
+
+const a = parseInt(process.argv[2]);
+const b = parseInt(process.argv[3]);
+
+console.log(add(a, b));
