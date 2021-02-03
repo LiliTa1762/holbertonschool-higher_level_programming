a6844080f2e143d1122713fcf183c021728a190a
Lilita1762 <lilibeth.tabares@gmail.com>
2021-02-03 02:21:33 +0000

    Taking argv from command line

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -3,7 +3,7 @@
 const myArgs = process.argv.slice(2);
 
 if (myArgs.length === 0){
-	console.log ('No argument');
+	console.log('No argument');
 }
 else if (myArgs.length === 1){
 	console.log('Argument found');
