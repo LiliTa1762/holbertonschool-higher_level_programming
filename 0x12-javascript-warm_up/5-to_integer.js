a211f8970f1e65081bdfa7bf628c81fa5084e712
Lilita1762 <lilibeth.tabares@gmail.com>
2021-02-03 04:05:53 +0000

    Parse to int

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 
 if (parseInt(process.argv[2])) {
-    console.log('My number: ' + process.argv[2]);
+  console.log('My number: ' + process.argv[2]);
 } else {
-    console.log('Not a number');
+  console.log('Not a number');
 }
