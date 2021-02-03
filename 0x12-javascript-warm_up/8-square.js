ea280ab6eb825dfc03fe1f78c2d56d3c2f868994
Lilita1762 <lilibeth.tabares@gmail.com>
2021-02-03 03:43:00 +0000

    Square in js. Fixed

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -3,9 +3,9 @@
 const argument = parseInt(process.argv[2]);
 
 if (argument) {
-    for (let x = 0; x < argument; x++) {
-	    console.log('X'.repeat(argument));
-	}
+  for (let x = 0; x < argument; x++) {
+  console.log('X'.repeat(argument));
+  }
 } else {
-    console.log('Missing size');
+  console.log('Missing size');
 }
