64a1959f5bb140e8a7ebb6e86db7543841a84ef2
Lilita1762 <lilibeth.tabares@gmail.com>
2021-02-03 03:16:08 +0000

    C is fun x times

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -4,7 +4,7 @@ let argument = parseInt(process.argv[2]);
 
 if (argument) {
     for (let i = 0; i < argument; i++) {
-	console.log('C is fun');
+    console.log('C is fun');
     }
 } else {
     console.log('Missing number of occurrences');
