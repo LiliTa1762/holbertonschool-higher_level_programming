bb49d4b45340bdd21d2769e64b20c233d68e6487
Lilita1762 <lilibeth.tabares@gmail.com>
2021-02-09 00:21:15 +0000

    Rectangle with 3 methods

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,30 @@
+#!/usr/bin/node
+
+module.exports = class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    } else {
+      return 'Rectangle {}';
+    }
+  }
+
+  print () {
+    const stringx = 'X';
+    for (let i = 0; i < this.height; i++) {
+      console.log(stringx.repeat(this.width));
+    }
+  }
+
+  rotate () {
+    const temp = this.height;
+    this.height = this.width;
+    this.width = temp;
+  }
+
+  double () {
+    this.width *= 2;
+    this.height *= 2;
+  }
+};
