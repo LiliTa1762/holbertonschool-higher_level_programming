63851aa5a2003e0a1ae7a897df171024b7165422
Lilita1762 <lilibeth.tabares@gmail.com>
2021-02-10 04:32:02 +0000

    Get body of url

---
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+
+const request = require('request');
+const fs = require('fs');
+const urlPath = process.argv[2];
+const storePath = process.argv[3];
+
+request.get(urlPath, function (err, res, body) {
+  if (err) console.log(err);
+  fs.writeFile(storePath, body, 'utf-8', (err) => {
+  if (err) console.log(err);
+  });
+});
