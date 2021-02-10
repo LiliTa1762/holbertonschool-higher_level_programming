6aaac2a2901319a1088619c88a7526dcdbf78d25
Lilita1762 <lilibeth.tabares@gmail.com>
2021-02-10 04:46:35 +0000

    Get body of url. Style checked

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -8,6 +8,6 @@ const storePath = process.argv[3];
 request.get(urlPath, function (err, res, body) {
   if (err) console.log(err);
   fs.writeFile(storePath, body, 'utf-8', (err) => {
-  if (err) console.log(err);
+    if (err) console.log(err);
   });
 });
